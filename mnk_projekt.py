ad48df69faaf1d0b680a7aae032de24b5345c3b4
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2024-01-15 16:01:41 +0100

    created log for games

--- mnk_projekt.py
+++ mnk_projekt.py
@@ -3,6 +3,7 @@ import random
 import time
 import math
 from datetime import datetime
+import csv
 
 
 class Board():
@@ -57,10 +58,17 @@ class Board():
                         return True
                 else:
                     count = 0
-                         
+        
+        # check for diagonal k long lines - Anton
+        # diagonal nach links
+        # wie beschreibt man eine linie die k lang ist und diagonal?
+        # starting point kann nur im bereich [m, m-k][n, n-k]
+
+
+        
+                
+
         return False
-
-
 
 class Player():
 
@@ -68,6 +76,7 @@ class Player():
         self.name = name
         self.player_number = player_number
         self.board = board
+        self.player_type = "human"
 
 
     def is_valid(self, move:tuple):
@@ -87,7 +96,6 @@ class Player():
         else:
             return False
 
-
     # !! man kann nur einen value error causen, bevor das Spiel abbricht :(
     def make_move(self): # -> (row, col)
         print(f"make move between 0 and {self.board.m-1} \nand 0 and {self.board.n-1}")
@@ -98,14 +106,13 @@ class Player():
         while not self.is_valid(move):
             print('Invalid move. Please try again')
             move = (int(input("Please make a move: ")), int(input("")))
-        return move
-        
+        return move    
 
 class Bot_random(Player):
 
     def __init__(self, player_number, name, board) -> None:
         super().__init__(player_number, name, board)
-
+        self.player_type = "bot_random"
 
     def make_move(self): # -> (row, col)
         """
@@ -128,6 +135,7 @@ class Bot_simple(Player):
 
     def __init__(self, player_number, name, board) -> None:
         super().__init__(player_number, name, board)
+        self.player_type = "bot_simple"
         pass
 
 
@@ -214,11 +222,11 @@ class Bot_simple(Player):
 
         pass
 
-
 class Bot_complex(Player):
 
     def __init__(self, player_number, name, board) -> None:
         super().__init__(player_number, name, board)
+        self.player_type = "bot_complex"
 
     def make_move(self): # -> (row, col)
         pass
@@ -232,6 +240,31 @@ class Game():
         self.player1 = player1
         self.player2 = player2   
         self.board = None
+        self.starting_player = None
+
+    def game_log(self):
+        """ # Main Game Log:
+        adds entry to dp (.csv) looking like this:
+
+        | player1_type | player2_type | starting_player | winning_player |   |
+        |--------------|--------------|-----------------|----------------|
+        | player       | player       | 1               | 0              |
+        | bot_random   | bot_random   | 2               | 1              |
+        | bot_simple   | bot_simple   | 1               | 2              |
+        | bot_complex  | bot_complex  | 2               | 0              |
+        """
+        with open("/home/arabanus/Documents/HSD/Prog DSKI/mnk_wise2324/game_log.csv", mode='a', newline="") as f:
+            fieldnames = ["player1_type", "player2_type", "starting_player", "winning_player"]
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            if f.tell() == 0:
+                writer.writeheader()
+            
+            writer.writerow({"player1_type": self.player1.player_type,
+                             "player2_type": self.player2.player_type,
+                             "starting_player": self.starting_player,
+                             "winning_player": self.winning_player})
+            f.close()
+
 
     def player_choice(self, p_number:int, p_name:str, choice:int):
         valid_choices = [1, 2, 3, 4]
@@ -239,30 +272,18 @@ class Game():
         if choice in valid_choices:
             if choice == 1:
                 player = Player(p_number, p_name, self.board)
-                # print("player is human")
-                # print(20*"-")
                 return player
             elif choice == 2:
                 player = Bot_random(p_number, p_name, self.board)
-                # print("player is a random bot")
-                # print(20*"-")
                 return player
             elif choice == 3:
                 player = Bot_simple(p_number, p_name, self.board)
-                # print("player is a simple bot")
-                # print(20*"-")
                 return player
             elif choice == 4:
                 player = Bot_complex(p_number, p_name, self.board)
-                # print("player is a complex bot")
-                # print(20*"-")
                 return player
             else:
                 raise ValueError("input number out of range, please retry!")
-
-    def log_moves(self):
-        now = datetime.now()
-        self.unique_game_id = now.strftime("%y%m%d_%H-%M-%S")
 
     
     def start(self, player1_type, player1_name, player2_type, player2_name):    
@@ -270,8 +291,8 @@ class Game():
 
         print(20*"-")
         
-        self.player1 = player1_name
-        self.player2 = player2_name
+        # self.player1 = player1_name
+        # self.player2 = player2_name
 
         self.player1 = self.player_choice(1, player1_name, player1_type)
         self.player2 = self.player_choice(2, player2_name, player2_type)
@@ -288,6 +309,13 @@ class Game():
     def game_loop(self):
         #made by Dalia
         current_player = random.choice([self.player1, self.player2])
+        
+        # only for log file: - Anton
+        if np.all(self.board == 0):
+            print("uyoasildgjkfbhvagluihsdkjf")
+            self.starting_player = current_player
+        
+
         while not self.full_board() and not self.board.has_won(current_player, self.k):
             self.board.display()
             print(f"Player {current_player.name}'s turn")
@@ -300,9 +328,11 @@ class Game():
             # checks if someone has won and if the board is full
             if self.board.has_won(current_player.player_number, self.k):
                 print(f"Player {current_player.name} wins!")
+                self.winning_player = current_player.player_number
                 break
             elif self.full_board():
                 print('The board is full. Nobody won!')
+                self.winning_player = 0
                 break
 
             # changes player
@@ -316,18 +346,15 @@ class Game():
         self.board.display()
 
 if __name__ == "__main__":
-    # for testing the script w/o gui and user input:
-    m = 6
-    n = 5
-    k = 4   
+    for i in range(20):
+        # for testing the script w/o gui and user input:
+        m = 6
+        n = 5
+        k = 4   
 
-    # human : 1
-    # bot random: 2
-    # bot simple: 3
-    # bot complex: 4
-    current_game = Game(m, n, k)
-    # human : 1, bot random: 2, bot simple: 3, bot complex: 4
-    current_game.start(player1_type=3, player1_name="simple",
-                       player2_type=2, player2_name="random")
-    # human : 1, bot random: 2, bot simple: 3, bot complex: 4
-    current_game.game_loop()
+        current_game = Game(m, n, k)
+        # human : 1, bot random: 2, bot simple: 3, bot complex: 4
+        current_game.start(player1_type=3, player1_name="simple",
+                           player2_type=2, player2_name="random")
+        current_game.game_loop()
+        current_game.game_log()
