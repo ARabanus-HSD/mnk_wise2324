70a377ac5f09c3e6e4c365f0f440782f97d73599
AntonRabanusvGreens <anton.rabanus@v-greens.com>
2024-01-21 13:43:28 +0100

    rudementary has won works!

--- random_testing.py
+++ random_testing.py
@@ -2,7 +2,7 @@ import numpy as np
 
 array = np.array([[0, 1, 0, 0, 0],
                   [0, 2, 0, 0, 0],
-                  [0, 0, 2, 1, 0],
+                  [1, 0, 2, 1, 0],
                   [0, 0, 1, 2, 1],
                   [0, 1, 0, 0, 2],
                   [1, 2, 2, 0, 0]])
@@ -59,10 +59,77 @@ for i in range(len(start_positions)):
 
 
 # new apporach:
-# make an array filled with 0 and a k long diagonal line of ones..
+# make an array filled with 0 and a k long diagonal line of ones.. done
 # add multiply the game board with this mask
 # add this mask to an array filled with the player number and 0 where the diagonal line is
 # if all entries in the array are the same, the diagonal line was a winning line
 # if not, shift array mask and try again
 
 # has to be done for \ and / lines!
+
+def d_line_rd(start_position, k): # \
+    
+    placements = []
+    for i in range(k):
+        placement = [start_position[0] + i, start_position[1] + i]
+        placements.append(placement)
+    return placements
+
+def d_line_ld(start_position, k): # /
+    placements = []
+    for i in range(k):
+        placement = [start_position[0] + i, start_position[1] - i]
+        placements.append(placement)
+        print(f"{i}: {placement}")
+    return placements
+
+def place_line_rd(arr, m, n, k, var, bg, line_var):
+    """input, array, m, n, k, and the variable that we're searching for
+    makes a mask in the top right, k away form the far edge and bottom because only there are possible starting points
+    """
+    print(arr)
+    arr[:, -(k-1):] = 0
+    arr[-(k-1):, :] = 0
+    print(arr)
+    past_moves = np.argwhere(arr == var)
+    
+    # place \ line
+    # only relevant starting positions are in the top left corner
+    # k distance from the right side, and the bottom
+    possible_lines = []
+    for i in range(len(past_moves)):
+        line_rd = d_line_rd(past_moves[i], k)
+        zero_mask = np.full((arr.shape[0], arr.shape[1]), bg, dtype=int)
+        for coord in line_rd:
+            print(coord[0], coord[1])
+            zero_mask[(coord[0], coord[1])] = line_var
+        possible_lines.append(zero_mask)
+    return possible_lines
+
+def has_won(game_board, list_of_lines, list_of_inverse_lines_w_player_number, player_number):
+    for i in range(len(list_of_lines)):
+        print(game_board)
+        comparison_step_1 = list_of_lines[i] * game_board
+        print(comparison_step_1)
+        comparison_step_2 = comparison_step_1 + list_of_inverse_lines_w_player_number[i]
+        print(comparison_step_2)
+        all_same = np.all(comparison_step_2==player_number)
+        return all_same
+    
+
+m, n ,k = (6, 5, 4)
+var = 1
+
+possible_lines = place_line_rd(array, m, n, k, var, 0, line_var=1)
+inverse_possible_lines_w_player_number = place_line_rd(array, m, n, k, var, var, line_var=0)
+print(possible_lines)
+print(inverse_possible_lines_w_player_number)
+
+array = np.array([[0, 1, 0, 0, 0],
+                  [0, 2, 0, 0, 0],
+                  [1, 0, 2, 1, 0],
+                  [0, 0, 1, 2, 1],
+                  [0, 1, 0, 0, 2],
+                  [1, 2, 2, 0, 0]])
+
+print(has_won(array, possible_lines, inverse_possible_lines_w_player_number, player_number=var))
